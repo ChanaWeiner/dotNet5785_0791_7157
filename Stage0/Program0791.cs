362a63ba2633bdd0a896d44608f2a5f9e9657856
ChanaWeiner <h85050909@gmail.com>
2024-09-22 16:22:51 +0300

    Initial Code + Welcome functions

---
+++ Stage0/Program0791.cs
@@ -0,0 +1,19 @@
+﻿partial class Program
+{
+    private static void Main(string[] args)
+    {
+        Welcome0791();
+        Welcome7157();
+        Console.ReadKey();
+
+    }
+
+    private static void Welcome0791()
+    {
+        Console.WriteLine("Enter your name:");
+        string name = Console.ReadLine();
+        Console.WriteLine(name + ", welcome to my console application");
+    }
+    static partial void Welcome7157();
+   
+}
