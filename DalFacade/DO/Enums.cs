235ed1010c603123988d0896187edc8e2b8d00b0
hodaya327 <hc0556752677@gmail.com>
2024-11-13 17:57:26 +0200

    add class

---
+++ DalFacade/DO/Enums.cs
@@ -0,0 +1,16 @@
+﻿namespace DO;
+//enum Role { MASTER_TUTOR , BEGINER_TUTOR, MANEGAR};
+public record Volunteer(
+    int Id,
+    string FullName,
+    string CellNumber,
+    string Email,
+    string Password,
+    string CurrentAddres,
+    //Role role
+    bool Active
+
+ )
+{
+    public Volunteer() : this(0,"","","","","",false) { }
+}
